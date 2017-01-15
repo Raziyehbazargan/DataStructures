0bcf477013cb305eee2f7b5ed312c6e39cb39f86
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-14 23:34:41 -0800

    fix - find duplcate in a string

--- intrview-questions/strings/find-Duplicate.js
+++ intrview-questions/strings/find-Duplicate.js
@@ -5,13 +5,19 @@
 function findDuplicate(str) {
   var counter = 1;
   var hashTable = {};
+  var result = {};
 
   for (var i = 0 ; i < str.length; i++) {
-    if (hashTable[str[i]]) {
-      hashTable[str[i]] = counter++;
+    if (hashTable.hasOwnProperty(str[i])) {
+      hashTable[str[i]] = hashTable[str[i]] + 1;
     } else {
       hashTable[str[i]] = counter;
     }
   }
-  return hashTable;
+
+  for (var key in hashTable) {
+    if ( hashTable[key] > 1)
+      result[key] = hashTable[key];
+  }
+  return result;
 }
