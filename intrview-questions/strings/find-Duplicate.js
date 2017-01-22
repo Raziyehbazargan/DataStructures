f9461a8dd8d239c10626a3b1169f34ba119a558c
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-22 00:18:04 -0800

    find missing number in ab=n array

---
+++ intrview-questions/strings/find-Duplicate.js
@@ -0,0 +1,24 @@
+// How to print duplicate characters from string?
+//example if String is "Java" then program should print "a".
+
+'use strict';
+
+function findDuplicate(str) {
+  var counter = 1;
+  var hashTable = {};
+  var result = {};
+
+  for (var i = 0 ; i < str.length; i++) {
+    if (hashTable.hasOwnProperty(str[i])) {
+      hashTable[str[i]] = hashTable[str[i]] + 1;
+    } else {
+      hashTable[str[i]] = counter;
+    }
+  }
+
+  for (var key in hashTable) {
+    if ( hashTable[key] > 1)
+      result[key] = hashTable[key];
+  }
+  return result;
+}
