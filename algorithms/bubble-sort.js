fc49c0601153d3127e084069c432bf78a688708d
raziyeh bazargan <r.bazargan@gmail.com>
2017-02-01 12:05:59 -0800

    bubble-sort

---
+++ algorithms/bubble-sort.js
@@ -0,0 +1,19 @@
+//Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+
+function bubbleSort(arr) {
+  var i,j;
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j);
+      }
+    }
+  }
+  return arr;
+}
+
+function swap(arr, j) {
+  var temp = arr[j];
+  arr[j] = arr[j + 1];
+  arr[j + 1] = temp;
+};
