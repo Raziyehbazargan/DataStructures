beaebe769a9049c84df23c443d49f2b9d673e34a
raziyeh bazargan <r.bazargan@gmail.com>
2017-02-28 10:36:30 -0800

    factorial - recursive and not-recursive

---
+++ interview-questions/strings/factorial.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Stack = require('../stack/implementation/stack/stack-2');
+
+//factorail 5! = 5 * 4 * 3 * 2 * 1 = 120
+// using recursive function
+
+function factorial(n) {
+  if (n == 0) return 1;
+  return n * factorial(n - 1);
+}
+
+// without recursive function
+/*
+
+function factorial(n) {
+  var result = 1;
+  var s = new Stack();
+  while (n > 1) {
+    s.push(n--);
+  }
+
+  while (!s.isEmpty()) {
+    result *= s.pop();
+  }
+  return result;
+}
+*/
