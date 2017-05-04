7d52d5ff4fe5ffcfd710df1314f444e1a1a5398a
raziyeh bazargan <r.bazargan@gmail.com>
2017-05-03 22:17:54 -0700

    palindrome - recursive

---
+++ 3-interview-questions/strings/palindrome-recursive.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function isPalindrome(str, start, end) {
+
+  if (start === end)
+    return true;
+
+  if (str[start] !== str[end])
+    return false;
+
+  if (s < e)
+    return isPalindrome(str, s + 1, e - 1);
+
+  return true;
+}
